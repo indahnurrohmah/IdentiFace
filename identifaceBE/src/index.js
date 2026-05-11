113ddb5a2f024d1f75c26e4c6b7579fdc4c68706
indahnurrohmah <indahnurrohmah@mail.ugm.ac.id>
2026-05-11 22:30:34 +0700

    feat: auth

--- identifaceBE/src/index.js
+++ identifaceBE/src/index.js
@@ -14,7 +14,13 @@ app.get('/api/health', (req, res) => {
     res.json({ status: 'success', message: 'IdentiFace Backend is running!' });
 });
 
-// API routes
+// API ROUTES
+
+// Import the auth routes
+const authRoutes = require('./routes/authRoutes');
+
+//  Mount the auth routes
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
