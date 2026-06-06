a7c90df9b7d2f055c6db5f601c87f3a6e01afc8a
indahnurrohmah <indahnurrohmah@mail.ugm.ac.id>
2026-06-06 09:14:15 +0700

    fix: change port

--- identifaceBE/src/index.js
+++ identifaceBE/src/index.js
@@ -64,6 +64,6 @@ app.use(errorHandler);
 
 // --- 7. START SERVER ---
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
